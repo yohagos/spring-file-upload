819babcd31b0388068ab14e911144d2e2a7fd72f
yohagos <yosef.hagos@googlemail.com>
2024-03-29 18:36:01 +0100

    feat: file upload example

---
+++ src/main/java/com/spring/fileupload/FileUploadApplication.java
@@ -0,0 +1,24 @@
+package com.spring.fileupload;
+
+import com.spring.fileupload.upload.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+}
